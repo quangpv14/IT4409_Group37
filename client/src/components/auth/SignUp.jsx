cbb5d370eb62e03f3943ac330e2d6b065ddf77ba
levansonyb <levansonyb2003@gmail.com>
2024-10-20 23:12:25 +0700

    test

--- client/src/components/auth/SignUp.jsx
+++ client/src/components/auth/SignUp.jsx
@@ -14,19 +14,63 @@ export const SignUp = () => {
     const [successMessage, setSuccessMessage] = useState("")
 
     const handleInPutChange = (e) => {
+        setRegister({ ...register, [e.target.name]: e.target.value })
     }
 
     const handleSignUp = async (e) => {
-
+        e.preventDefault()
+        try {
+            const result = await signUp(register)
+            setSuccessMessage(result)
+            setErrorMessage("")
+            setRegister({ firstName: "", lastName: "", email: "", password: "" })
+        } catch (error) {
+            setSuccessMessage("")
+            setErrorMessage(`Error ${error.message}`)
+        }
+        setTimeout(() => {
+            setErrorMessage("")
+            setSuccessMessage("")
+        }, 5000)
     }
 
     return (
         <section className='container col-6 mt-5 mb-5'>
             {errorMessage && <p className='alert alert-danger'>{errorMessage}</p>}
             {successMessage && <p className='alert alert-success'>{successMessage}</p>}
-
+            <h2 className='text-center'>Sign Up</h2>
             <form onSubmit={handleSignUp}>
+                <div className='mb-3 row'>
+                    <label htmlFor="firstName" className='col-sm-2 col-form-label'>First Name</label>
+                    <div className='col-sm-10'>
+                        <input type="text" id='firstName' name='firstName' className='form-control'
+                            required value={register.firstName} onChange={handleInPutChange} />
+                    </div>
+                </div>
 
+                <div className='mb-3 row'>
+                    <label htmlFor="lastName" className='col-sm-2 col-form-label'>Last Name</label>
+                    <div className='col-sm-10'>
+                        <input type="text" id='lastName' name='lastName' className='form-control'
+                            required value={register.lastName} onChange={handleInPutChange} />
+                    </div>
+                </div>
+
+                <div className='mb-3 row'>
+                    <label htmlFor="email" className='col-sm-2 col-form-label'>Email</label>
+                    <div className='col-sm-10'>
+                        <input type="email" id='email' name='email' className='form-control'
+                            required value={register.email} onChange={handleInPutChange} />
+                    </div>
+                </div>
+
+                <div className='mb-3 row'>
+                    <label htmlFor="password" className='col-sm-2 col-form-label'>Password</label>
+                    <div className='col-sm-10'>
+                        <input type="password" id='password' name='password' className='form-control'
+                            required value={register.password} onChange={handleInPutChange} />
+                    </div>
+                </div>
                 <div className='mb-3'>
                     <button type='submit' className='btn btn-danger' style={{ marginRight: "10px" }}>
                         Sign Up
